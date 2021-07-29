64cfed4cd3a4208e774e263fa46ab865c9008b73
Radu Tirca <radu@pythia.social>
2021-07-30 02:31:19 +0300

    Fixed issues

--- init-setup.sh
+++ init-setup.sh
@@ -6,14 +6,15 @@ echo "Installing starship..."
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 echo "[-] Download fonts [-]"
 echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip"
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+wget -N https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
 unzip FiraCode.zip -d ~/.fonts
 fc-cache -fv
 echo "Done! Fira code installed"
 echo "Installing zsh-autosuggestions..."
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 echo "Adding zsh-autosuggestions to zsh plugins..." 
-sed -i 's/# plugins=(git)/plugins=(git zsh-autosuggestions)/' ~/.zshrc
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/' ~/.zshrc
+echo "eval \"\$(starship init zsh)\"" >> ~/.zshrc
 source ~/.zshrc
 echo "Downloading starship config file..."
 wget -p ~/.config/ https://raw.githubusercontent.com/radu-pythia/init-setup/main/starship.toml
