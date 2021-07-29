5dcd48517fd6c972d5ac6b871d23da67888bb6f0
Radu Tirca <radu@pythia.social>
2021-07-30 02:19:56 +0300

    Initial commit

--- init-setup.sh
+++ init-setup.sh
@@ -1,16 +1,20 @@
 #! /bin/bash
-sudo apt-get install git zsh curl
+sudo apt-get install git zsh curl wget
 echo "Installing oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "Installing starship..."
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
-echo "Installing zsh-autosuggestions..."
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 echo "[-] Download fonts [-]"
 echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip"
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
 unzip FiraCode.zip -d ~/.fonts
 fc-cache -fv
-echo "done! Fira code installed"
-sed -i 's/# plugins=*/plugins=(git zsh-autosuggestions)' ~/.zshrc
+echo "Done! Fira code installed"
+echo "Installing zsh-autosuggestions..."
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo "Adding zsh-autosuggestions to zsh plugins..." 
+sed -i 's/# plugins=(git)/plugins=(git zsh-autosuggestions)/' ~/.zshrc
+source ~/.zshrc
+echo "Downloading starship config file..."
+wget -p ~/.config/ https://raw.githubusercontent.com/radu-pythia/init-setup/main/starship.toml
 echo "Hopefully done!"
